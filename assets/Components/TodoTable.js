ec0a1c1b529982ce96272cc69fb92f221b4210af
AIZEN <welsa250@gmail.com>
2023-08-14 11:18:56 -1100

    Created a context and rendered the app unto the template

---
+++ assets/Components/TodoTable.js
@@ -0,0 +1,15 @@
+import React, {useContext} from 'react';
+import {TodoContext} from "../Context/TodoContext";
+
+function TodoTable() {
+    const context = useContext(TodoContext)
+    return (
+        <div>
+            {context.todos.map(todo =>(
+                <div>{todo.name}</div>
+            ))}
+        </div>
+    );
+}
+
+export default TodoTable;
